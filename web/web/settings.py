4d4c831bc3ee51693cec56dd7f0d63663460a3fe
munichstyles <munichstyles@users.noreply.github.com>
2022-06-21 13:49:47 +0200

    requiments

--- web/web/settings.py
+++ web/web/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '/django.frankpartners.eu/web/db.sqlite3'),
     },
 }
 
